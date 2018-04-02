cc2e39d43bf06d73fb40380aa9fdb7f5bd293024
shinyatime <shinyatan@sina.com>
2018-04-02 21:06:45 +0800

    服务提供者，服务消费者接口

---
+++ microservice-provider-user/src/main/java/com/intan/cloud/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.intan.cloud.repository;
+
+import com.intan.cloud.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ microservice-provider-user/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user(id, username, name, age, balance) values(1, 'user1', '张三', 20, 100.00);
+insert into user(id, username, name, age, balance) values(2, 'user2', '李四', 21, 110.00);
+insert into user(id, username, name, age, balance) values(3, 'user3', '王五', 22, 120.00);
+insert into user(id, username, name, age, balance) values(4, 'user4', '赵六', 23, 130.00);
+insert into user(id, username, name, age, balance) values(5, 'user5', '田七', 24, 140.00);
