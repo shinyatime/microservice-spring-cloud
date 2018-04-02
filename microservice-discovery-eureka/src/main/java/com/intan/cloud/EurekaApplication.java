5c48ab8a59e071b1b601d4cf46695f30471e995c
shinyatime <shinyatan@sina.com>
2018-04-02 17:31:50 +0800

    初始化项目

---
+++ microservice-discovery-eureka/src/main/java/com/intan/cloud/EurekaApplication.java
@@ -0,0 +1,13 @@
+package com.intan.cloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
